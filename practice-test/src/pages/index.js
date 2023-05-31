9f34bb54b3357dd214871c6440ba56174d42a81d
karla-mond <karla.mondragon.rosas@gmail.com>
2023-05-30 18:08:51 -0600

    informacion index.js

--- practice-test/src/pages/index.js
+++ practice-test/src/pages/index.js
@@ -1,7 +1,8 @@
 export default function Home() {
     return (
         <>
-        Hola Mundo! Soy Karla
+        <h2> Hola Mundo! Soy Karla! </h2>
+        <h3> Este es un ejercicio práctica en el que voy a intentar hacer un workflow para una lambda para el endpoint: /api/hello </h3>
         </>
     );
 }
