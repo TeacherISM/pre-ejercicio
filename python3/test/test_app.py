35101fc7f30e4e1335d3c7afafdeda6b5deed765
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-05-30 18:22:15 -0600

    Improved coverage

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -5,9 +5,15 @@ sys.path.append('../src')
 
 class AppTest(TestCase):
     def setUp(self):
-        self.app = app.app.test_client()
+        self.app = app.test_client()
 
     def test_home(self):
-        result = self.app.get('/')
-        self.assertEqual(result.status_code, 200)
-        self.assertIn(b"Hello, Luis Javier!", result.data)
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Hello, Luis Javier!", response.data)
+
+    def test_welcome(self):
+        response = self.app.get('/welcome')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>Title</title>\n</head>\n<body>\nHello Luis Javier!\n</body>\n</html>')
+
