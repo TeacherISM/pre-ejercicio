3cadbc2c0577a76452fae89c938a1c900446dd97
Salvador Federico Milanes Braniff <90704785+SFMBa01029956@users.noreply.github.com>
2023-05-30 20:40:18 -0600

    Change js for python implementation

---
+++ python3/tests/lambdaTest.py
@@ -0,0 +1,18 @@
+import sys
+from src import app
+# from unittest import TestCase # Importing the TestCase class
+sys.path.append('../src')
+
+# # Using unittest
+# class Test(TestCase):
+#     # Check lambda for response 'Salva Milanés'
+#     def test_lambda_handler(self):
+#         result = app.lambda_handler(None, None)
+#         self.assertEqual(result, 'Salva Milanés')
+
+# Using pytest
+def test_lambda_handler():
+    result = app.lambda_func(None, None)
+    assert result == 'Salva Milanés'
+
+
