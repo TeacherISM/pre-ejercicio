725fbee7be8b56570cbb9298784bf56e21b1c58f
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-05-30 18:18:34 -0600

    Fixed some paths

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -1,19 +1,13 @@
-import unittest
-from flask import Flask, render_template
+from src import app
+from unittest import TestCase
 import sys
 sys.path.append('../src')
 
-class FlaskRenderingTestCase(unittest.TestCase):
+class AppTest(TestCase):
     def setUp(self):
-        self.app = Flask(__name__)
-        self.app.testing = True
+        self.app = app.app.test_client()
 
-    def test_home_page_rendering(self):
-        with self.app.test_request_context('/'):
-            response = self.app.dispatch_request()
-            self.assertEqual(response.status_code, 200)
-            self.assertIn(b'Hello, Luis Javier!', response.data)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_home(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"Hello, Luis Javier!", result.data)
