bdd8c1e35d7acc7d347870e03108cdee0fdc85eb
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-05-30 18:24:10 -0600

    Fixed test :D

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -5,15 +5,14 @@ sys.path.append('../src')
 
 class AppTest(TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.app = app.app.test_client()
 
     def test_home(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Hello, Luis Javier!", response.data)
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"Hello, Luis Javier!", result.data)
 
     def test_welcome(self):
         response = self.app.get('/welcome')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode('utf-8'), '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <title>Title</title>\n</head>\n<body>\nHello Luis Javier!\n</body>\n</html>')
-
