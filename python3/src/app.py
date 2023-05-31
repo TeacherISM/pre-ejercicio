990a73c418d913fb9422aea653d6335f565dd789
Salvador Federico Milanes Braniff <90704785+SFMBa01029956@users.noreply.github.com>
2023-05-30 20:40:29 -0600

    Remove js files

---
+++ python3/src/app.py
@@ -0,0 +1,2 @@
+def lambda_func(event, context):
+  return 'Salva Milanés'
--- src/app/show.test.js
+++
@@ -1,9 +0,0 @@
-import app from '../app'
-import request from 'supertest'
-
-describe('show', () => {
-  test('Api responds with 200', async () => {
-    const response = await request(app).get('/api/hello')
-    expect(response.statusCode).toBe(200)
-  })
-})
