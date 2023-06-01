09cd7c214f5f292013dc6c66dd41a3c2f0a718ea
reginardzss <84197750+reginardzss@users.noreply.github.com>
2023-06-01 11:05:16 -0600

    updated app, test and workflow

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -3,34 +3,7 @@ from unittest import TestCase
 import sys
 sys.path.append('../src')
 
-
 class AppTest(TestCase):
     def test_home(self):
-        result = app.home()
-        self.assertEqual(result, "Hello, World!")
-
-    def setUp(self):
-        self.app = app.app.test_client()
-
-    def test_welcome(self):
-        result = self.app.get('/welcome')
-        self.assertIn(b"WELCOME!", result.data)
-
-    def test_countries(self):
-        result = self.app.get('/countries')
-        self.assertIn(b"Thailand", result.data)
-        self.assertIn(b"Australia", result.data)
-        self.assertIn(b"Egypt", result.data)
-
-    def test_add_country(self):
-        result = self.app.post('/countries', json={"name": "New Zealand", "capital": "Wellington", "area": 268021})
-        self.assertIn(b"New Zealand", result.data)
-        self.assertIn(b"Wellington", result.data)
-        self.assertIn(b"268021", result.data)
-        self.assertIn(b"4", result.data)
-        self.assertEqual(result.status_code, 201)
-
-    def test_add_country_invalid(self):
-        result = self.app.post('/countries', data="This is not JSON")
-        self.assertIn(b"Request must be JSON", result.data)
-        self.assertEqual(result.status_code, 415)
+        result = app.handler()
+        self.assertEqual(result, "Hola Regina")
