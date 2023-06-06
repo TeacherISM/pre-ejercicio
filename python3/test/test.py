bb376142f0b4f9f7438b0f30e92654f66749960a
Andres Briseno <A01352283@tec.mx>
2023-06-06 01:35:21 -0600

    Setting up "build"

---
+++ python3/test/test.py
@@ -0,0 +1,14 @@
+import unittest
+from src import handler
+
+class TestHandler(unittest.TestCase):
+    def test_handler(self):
+        # Arrange
+        event = {}
+        context = {}
+
+        # Act
+        result = handler(event, context)
+
+        # Assert
+        self.assertEqual(result, "Andrés says: Hello World!")
