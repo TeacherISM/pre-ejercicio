b2c1206761bd8fff53bb4af4525eed7824f6fa30
Andreina Sananez <asananez2000@gmail.com>
2023-06-06 10:10:56 -0600

    update workflow

--- app/src/app.py
+++ app/src/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import aws_lambda_wsgi 
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hola Andreina'
+
+def lambda_handler(event, context):
+    return aws_lambda_wsgi.response(app, event, context)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/src/test_app.py
+++ app/src/test_app.py
@@ -1,6 +1,6 @@
 import pytest
 from flask_testing import TestCase
-from app import app
+from app import app, lambda_handler
 
 class MyTest(TestCase):
     def create_app(self):
@@ -10,3 +10,26 @@ class MyTest(TestCase):
         response = self.client.get('/')
         self.assert200(response)  # assert the response status code is 200
         self.assertEqual(response.data.decode(), 'Hola Andreina')
+
+def test_lambda_handler():
+    event = {
+        "httpMethod": "GET",
+        "path": "/",
+        "queryStringParameters": None, 
+        'body': None,
+        'resource': '/',
+        'requestContext': {
+            'resourcePath': '/',
+            'httpMethod': 'GET',
+            'path': '/',
+        },
+        'headers': {
+            'X-Forwarded-Proto': 'https',
+        }
+    }
+    context = {}  # context can be an empty dictionary in most cases
+
+    response = lambda_handler(event, context)
+    
+    assert response['statusCode'] == 200
+    assert response['body'] == "Hola Andreina"
