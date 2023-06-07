e3f0c6c04745e9de30f1d8584c173b4adc4d5248
A01352283 <a01352283@tec.mx>
2023-06-06 23:39:14 -0600

    Removes my files from main

--- python3/execute.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python ./src/app.py run -h 0.0.0.0 -p 5000
--- python3/test/test_app.py
+++
@@ -1,7 +0,0 @@
-# test_hello_world.py
-import pytest
-from src import app
-
-def test_lambda_handler():
-    response = app.handler(None, None)
-    assert response == "Hello, world! A01352283"
